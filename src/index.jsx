d7e6a2c4bb95d3a365f554f4d122a2809860f6df
Jérôme Guèrin <guerin_jerome@icloud.com>
2023-06-23 11:13:14 +0200

    playground with react context

---
+++ src/index.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import AppWithContext from "./AppWithContext";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppWithContext />);
